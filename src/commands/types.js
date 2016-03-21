5e132eb4ac5dd77c931941dcbaf7028d081f4641
Autarc <autarc@gmail.com>
2016-03-22 00:01:56 +0100

    add command 'types'

---
+++ src/commands/types.js
@@ -0,0 +1,19 @@
+/**
+ * # Command: Tasks
+ *
+ *
+ */
+
+import { Socket } from '../utilities/network'
+import chalk from 'chalk'
+
+export default {
+  command: 'types',
+  alias: ['tasks', 'list'],
+  description: 'List the types of tasks which can be processed',
+  action ({ socket }) {
+    return socket.once('/tasks/types').then((types) => {
+      return types.map((type) => `${chalk.yellow(type.name)}: ${type.text}`)
+    })
+  }
+}
